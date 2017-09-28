9c6b37f88425b07a1d0d4c8518c0763c5f76714d
devxris <chuwei0915@gmail.com>
2017-09-28 22:14:42 +0800

    Implement device and video output with AVCaptureSession and AVCaptureDevice and AVCaptureMovieFileOutput

--- FullScreenVideoRecorder/Controller/VideoRecorderViewController.swift
+++ FullScreenVideoRecorder/Controller/VideoRecorderViewController.swift
@@ -7,13 +7,66 @@
 //
 
 import UIKit
+import AVFoundation
 
 class VideoRecorderViewController: UIViewController {
 
 	@IBOutlet weak var cameraButton: UIButton!
 	
-	@IBAction func capture(_ sender: UIButton) {
+	lazy var captureSession: AVCaptureSession = {
+		let session = AVCaptureSession()
+		session.sessionPreset = AVCaptureSession.Preset.high
+		return session
+	}()
+	
+	// Input Device
+	var currentDevice: AVCaptureDevice?
+	// Video output
+	var videoOutput: AVCaptureMovieFileOutput?
+	// Creating a preview layer
+	var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// Selecting input device
+		if let device = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) {
+			currentDevice = device
+		} else if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+			currentDevice = device
+		}
+		
+		// Get the input data source
+		guard let captureDeviceInput = try? AVCaptureDeviceInput(device: currentDevice!) else { return }
+		
+		// Configure AVCaptureMovieFileOutput
+		videoOutput = AVCaptureMovieFileOutput()
+		
+		// Configure the session with the input and the output devices
+		if captureSession.canAddInput(captureDeviceInput) {
+			captureSession.addInput(captureDeviceInput)
+			if captureSession.canAddOutput(videoOutput!) {
+				captureSession.addOutput(videoOutput!)
+			} else {
+				print("captureSession can't add output")
+			}
+		} else {
+			print("captureSession can't add input")
+		}
+		
+		// Configure camera preview layer
+		cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+		view.layer.addSublayer(cameraPreviewLayer!)
+		cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+		cameraPreviewLayer?.frame = view.layer.frame
+		
+		// Bring the camera button to front
+		view.bringSubview(toFront: cameraButton)
+		
+		// Start captureSession
+		captureSession.startRunning()
 	}
+	
 	
 	@IBAction func unwind(segue: UIStoryboardSegue) {
 	}
