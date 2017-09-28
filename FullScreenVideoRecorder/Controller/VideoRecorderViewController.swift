67461fc7876345a759d4f467d272c58065f47198
devxris <chuwei0915@gmail.com>
2017-09-28 12:39:54 +0800

    Simple UI and setup

---
+++ FullScreenVideoRecorder/Controller/VideoRecorderViewController.swift
@@ -0,0 +1,20 @@
+//
+//  VideoRecorderViewController.swift
+//  FullScreenVideoRecorder
+//
+//  Created by Chris Huang on 28/09/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class VideoRecorderViewController: UIViewController {
+
+	@IBOutlet weak var cameraButton: UIButton!
+	
+	@IBAction func capture(_ sender: UIButton) {
+	}
+	
+	@IBAction func unwind(segue: UIStoryboardSegue) {
+	}
+}
